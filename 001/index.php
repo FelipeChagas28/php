53faa8706025f48f10a960e238af38a33d3c3cd2
Felipe <felipeschagas28@gmail.com>
2025-06-01 22:59:32 -0300

    002

--- 001/index.php
+++ 001/index.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Teste</title>
 </head>
 <body>
     <h1>
         <?php 
-            echo "Não sei";
+            echo "super tag PHP";
         ?>
     </h1>
+
+    <h1>
+        <? echo "Short open tag"; ?>
+    </h1>
+
+    <h1>
+        <!-- Essa não funciona nas versões recentes
+            <% echo "ASP tag"; %> 
+        -->
+    </h1>
+
+    <h1>
+        <!-- A tag mais usada -->
+        <?=  "<p>Short tag PHP</p>" ?>
+    </h1>
+
+    <?php 
+        $nome = "Felipe";
+        echo "$nome"
+    ?>
 </body>
 </html>
